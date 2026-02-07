f96172e4da31c1cc9a549c0621c3e5d6ec7cd1ac
比乐喵 <17762591607@163.com>
2026-02-08 00:45:11 +0800

    添加CRT 之前

--- Assets/Scripts/Tiles/OrangeTile.cs
+++ Assets/Scripts/Tiles/OrangeTile.cs
@@ -1,18 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OrangeTile : MonoBehaviour
+public class OrangeTile : BaseAttackTile
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Tiles/RedTile.cs
+++ Assets/Scripts/Tiles/RedTile.cs
@@ -5,43 +5,6 @@ using UnityEngine;
 /// 红色方块，本身作为最基础炮台使用，从方块内部发射
 /// </summary>
 
-public class RedTile : Tile
+public class RedTile : BaseAttackTile
 {
-    [SerializeField] private Bullet bullet;
-
-    [Header("开火设置")]
-    [SerializeField] protected AudioSource _FireSound;
-    protected float _weaponShootCDTimer;
-    [SerializeField] protected float _weaponShootCD = 0.5f;
-    [SerializeField] private float _recoilForce = 5f; // 基础后坐力
-
-
-    protected override void Awake()
-    {
-        _color = Color.red;
-        _tileType = TileType.Attack;
-        _FireSound = GetComponent<AudioSource>();
-        base.Awake();
-    }
-
-    protected override void Update()
-    {
-        base.Update();
-        _weaponShootCDTimer += Time.deltaTime;
-    }
-
-    public void Shoot(Vector2 direction)
-    {
-        if (_weaponShootCDTimer >= _weaponShootCD && _ship._energy >= _energyConsume)
-        {
-            var newBullet = Instantiate<Bullet>(bullet, transform.position, Quaternion.identity);
-            newBullet._direction = direction;
-            newBullet._shooter = _ship;
-            _weaponShootCDTimer = 0;
-            _ship.ApplyRecoil(direction,1f,_recoilForce);
-            _FireSound.Play();
-
-            _ship.ConsumeEnergy(_energyConsume);
-        }
-    }
 }
